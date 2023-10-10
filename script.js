d111eb120486a34cffc4ba132b53104926a1588d
Shun Zhang <38138050+McZure@users.noreply.github.com>
2023-10-10 19:10:08 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,13 @@
-class ShapeSystem{
-  constructor(){
-  
-  }
-  
-  createShape(){
-
-  }
-  
-}
-let system = new ShapeSystem()
+setTimeout(
+  function(){
+    console.log("hello, delayed function")
+  },
+)
 function setup() {
   createCanvas(600, 600)
-  console.log("2 + 2 =", sum(2, 2))
 }
 
 
 function draw() {
   background(200)
-  ellipse(200, 100, 100, 150)
-}
-
-function sum(a, b) {
-  return a + b;
 }
