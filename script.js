3bc17cdb566b2ed7c5b9759654cf485d74a5854d
Shun Zhang <38138050+McZure@users.noreply.github.com>
2023-10-10 19:23:14 -0400

    class notes

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
+let bg=[0, 0, 0]
+myBool = false
+
+function callFirst(){
+  console.log("hello, delayed function")
+  bg = [200, 200, 200]
+}
+setTimeout(callFirst, 2000) //延迟2000ms运行function
+
 setTimeout(
   function(){
-    console.log("hello, delayed function")
-  },
-)
+    console.log("hello, again!")
+    bg = [100, 100, 100]
+    myBool = true
+  }, 4000)
 function setup() {
   createCanvas(600, 600)
 }
 
 
 function draw() {
-  background(200)
+  background(bg)
+  if(myBool){}
 }
