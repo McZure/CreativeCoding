0dae5dbe67fafe3a9f19d49cfed0178839635ecf
Shun Zhang <38138050+McZure@users.noreply.github.com>
2023-10-16 19:20:13 -0400

    Class notes

--- shape.js
+++ shape.js
@@ -1,9 +1,13 @@
+let hue = chroma.hsl(0, 1, 0.5)
+// HUE 0 ... 360
+// SAT 0 ... 1
+// 
 function setup() {
     createCanvas(600, 600)
-    console.log("2 + 2 =", sum(2, 2))
   }
   function draw() {
-    background(200)
+    hue = chroma.hsl(frameCount % 360, 1, 0.5)
+    background(hue.rgb())
     ellipse(400, 100, 100, 150)
   }
   
