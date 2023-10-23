434705715ac5ecefb3442239342dd75485dd9cfc
Shun Zhang <38138050+McZure@users.noreply.github.com>
2023-10-23 12:32:31 -0400

    Update shape.js

--- shape.js
+++ shape.js
@@ -2,11 +2,25 @@ let hue = chroma.hsl(0, 1, 0.5)
 let scale = chroma.scale(["yellow", "blue"])
 let shape = {
   x: 0,
-  y: 0,
+  y: 350,
   w: 100,
   h: 100,
   c: [0, 0, 200]
 }
+let tween = p5.tween.manager
+  .addTween(shape, "shapeTween")
+  // .addMotion("w", 12, 1000, "easeInSin")
+  // .addMotion("x", 500, 1000, "easeOutSin")
+  // .addMotions([
+  //   {key: "w", target: 12},
+  //   {key: "x", target: 500}
+  // ], 1000, "easeInSin")
+  // .startLoop() //startTween()
+
+let huetween = p5.tween.manager
+  .addTween(shape, "shapeTween")
+
+console.log(p5.tween.manager)
 // HUE 0 ... 360
 // Saturation 0 ... 1
 // Lightness 0 ... 1
@@ -32,4 +46,23 @@ function setup() {
     rect(shape.x, shape.y, shape.w, shape.h)
   }
   
+  function mouseClicked(){
+    console.log("click")
+
+    tween.resetMotions()
+
+    tween
+      .addMotions([
+        {key: "x", target: mouseX},
+        {key: "y", target: mouseY}
+      ], 1000, "easeInOutQuint")
+      // .addMotion("x", mouseX, 100, "easeInSin")
+      // .addMotion("y", mouseY, 100, "easeInSin")
+      .startTween()
+
+    huetween.resetMotions()
+    huetween
+      .addMotion("c", scale(mouseX/width).rgb(), 1000, "easeInOUtQuint")
+      .startTween()
+  }
   // chroma.scale(['yellow', 'blue']) 制作渐变色
