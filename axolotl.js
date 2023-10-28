68de3584440c31ee27fd14c8d8e54956567c53ec
Shun Zhang <38138050+McZure@users.noreply.github.com>
2023-10-28 00:31:57 -0400

    Improvement
    update several new types of eyes; enhance several old version of components

--- axolotl.js
+++ axolotl.js
@@ -24,6 +24,7 @@ class axolotl{
   gill(gill_type, _clr1, _clr2, face_clr){
     noStroke()
     fill(_clr1[0],_clr1[1],_clr1[2])
+    var hue = chroma.hsl(frameCount % 360, 1, 0.8)
     var slight_Xm = map(mouseX - this.x, -width/2, width/2, -5, 5, true)
     var slight_Ym = map(mouseY - this.y, -height/2, height/2, -5, 5, true)
     if (gill_type%7 == 0){
@@ -48,7 +49,7 @@ class axolotl{
     }
     else if(gill_type%7 == 1){
       // 2nd type of gill
-      // =(0_0)=
+      // ≡(0_0)≡
       quad(this.x-168 + slight_Xm, this.y-30, this.x+168 - slight_Xm, this.y-30, this.x+152 - slight_Xm, this.y-10, this.x-152 + slight_Xm, this.y-10)
       quad(this.x-150 - slight_Xm, this.y+28, this.x+150 + slight_Xm, this.y+28, this.x+166 + slight_Xm, this.y+10, this.x-166 - slight_Xm, this.y+10)
       fill(_clr2[0],_clr2[1],_clr2[2])
@@ -56,7 +57,7 @@ class axolotl{
     }
     else if(gill_type%7 == 2){
       // 3rd type of gill
-      // >>-(0_0)-<<
+      // ⋙(0_0)⋘
       push()
       translate(this.x, this.y)
       var angle1 = map(mouseY, -height/2, height/2, -2*PI/10, PI/10, true)
@@ -91,7 +92,7 @@ class axolotl{
     }
     else if(gill_type%7 == 3){
       // 4th type of gill
-      // ((0_0))
+      // ◖(0_0)◗
       ellipse(this.x+80, this.y+slight_Ym, 120+2*slight_Xm)
       ellipse(this.x-80, this.y+slight_Ym, 120+2*slight_Xm)
       stroke(face_clr[0],face_clr[1],face_clr[2])
@@ -107,7 +108,7 @@ class axolotl{
     }
     else if(gill_type%7 == 4){
       // 5th type of gill
-      // \(0_0)/
+      // ◥(0_0)◤
       push()
       translate(this.x, this.y)
       rotate(PI/15)
@@ -129,19 +130,21 @@ class axolotl{
     }
     else if(gill_type%7 == 5){
       // 6th type of gill
-      // *(0_0)*
+      // ►(0_0)◄
       push()
       translate(this.x, this.y)
-      fill(random(110, 210),random(110, 210),random(110, 210))
+      fill(hue.rgb())
       triangle(0, 0, -190 + slight_Xm, -45+ slight_Ym, -170+ slight_Xm, -80)
       fill(_clr1[0],_clr1[1],_clr1[2])
       triangle(0, 0, -200 + slight_Xm, -20+ slight_Ym, -200+ slight_Xm, 20)
-      fill(random(110, 210),random(110, 210),random(110, 210))
+      hue = chroma.hsl(20+frameCount % 360, 1, 0.8)
+      fill(hue.rgb())
       triangle(0, 0, -190 + slight_Xm, 40+ slight_Ym, -170+ slight_Xm, 70)
 
       fill(_clr1[0],_clr1[1],_clr1[2])
       triangle(0, 0, 190 + slight_Xm, -45+ slight_Ym, 170+ slight_Xm, -80)
-      fill(random(110, 210),random(110, 210),random(110, 210))
+      hue = chroma.hsl(40+frameCount % 360, 1, 0.8)
+      fill(hue.rgb())
       triangle(0, 0, 200 + slight_Xm, -20+ slight_Ym, 200+ slight_Xm, 20)
       fill(_clr1[0],_clr1[1],_clr1[2])
       triangle(0, 0, 190 + slight_Xm, 40+ slight_Ym, 170+ slight_Xm, 70)
@@ -149,7 +152,7 @@ class axolotl{
     }
     else if(gill_type%7 == 6){
       // 7th type of gill
-      // 3(0_0)3
+      // ʚ(0_0)ɞ
       push()
       translate(this.x, this.y)
       beginShape()
@@ -174,16 +177,17 @@ class axolotl{
   
   eyes(eyes_type, _clr, face_clr){
     noStroke()
+    var hue = chroma.hsl(frameCount % 360, 1, 0.8)
     var slight_Xm = map(mouseX - this.x, -width/2, width/2, -5, 5, true)
     var slight_Ym = map(mouseY - this.y, -height/2, height/2, -5, 5, true)
     var toEyeDist = dist(mouseX, mouseY, this.x, this.y)
     // *Use this variable to calculate the highlight size in the eyes
     var highlight = min(dist(mouseX, mouseY, width, 0), dist(mouseX, mouseY, 0, height), dist(mouseX, mouseY, 0, 0), dist(mouseX, mouseY, height, width))
     
-    if(eyes_type%7 == 0){
+    if(eyes_type%10 == 0){
       // 1st type of eyes
       // Peaceful eyes
-      // (Θ Θ)
+      // (⊝⊝)
       push()
       translate(this.x, this.y)
       fill("white")
@@ -194,10 +198,10 @@ class axolotl{
       rect(10+slight_Xm, -26+slight_Ym, 35, 23 - toEyeDist/50)
       pop()
     }
-    else if(eyes_type%7 == 1){
+    else if(eyes_type%10 == 1){
       // 2nd type of eyes
       // Cat eyes
-      // (0 0)
+      // (◉◉)
       push()
       translate(this.x, this.y)
       fill("white")
@@ -211,10 +215,10 @@ class axolotl{
       ellipse(30+slight_Xm, -25+slight_Ym, highlight/30)
       pop()
     }
-    else if(eyes_type%7 == 2){
+    else if(eyes_type%10 == 2){
       // 3rd type of eyes
       // Big eyes
-      // (D D)
+      // (⋒ ⋒)
       push()
       translate(this.x, this.y)
       fill(_clr[0]/2, _clr[1]/2, _clr[2]/2)
@@ -231,7 +235,7 @@ class axolotl{
       ellipse(35+slight_Xm, -35+slight_Ym, 10)
       pop()
     }
-    else if(eyes_type%7 == 3){
+    else if(eyes_type%10 == 3){
       // 4th type of eyes
       // Cynical eyes
       // (￢ ￢)
@@ -251,7 +255,7 @@ class axolotl{
       rect(-82, -72, 165, 30, 10, 10, 0, 0)
       pop()
     }
-    else if(eyes_type%7 == 4){
+    else if(eyes_type%10 == 4){
       // 5th type of eyes
       // Cyclops
       // ( ◕ )
@@ -267,10 +271,10 @@ class axolotl{
       ellipse(-20, -70, 60)
       pop()
     }
-    else if(eyes_type%7 == 5){
+    else if(eyes_type%10 == 5){
       // 6th type of eyes
       // Eyes behind glasses
-      // (@-@)
+      // (〄〄)
       push()
       translate(this.x, this.y)
       fill(_clr[0]/2, _clr[1]/2, _clr[2]/2)
@@ -293,41 +297,54 @@ class axolotl{
       rect(20-slight_Xm, -20-toEyeDist/10, 5, 5)
       pop()
     }
-    else if(eyes_type%7 == 6){
+    else if(eyes_type%10 == 6){
       // 7th type of eyes
       // Electronic eyes
-      // ([''])
+      // (▦▦)
       push()
       translate(this.x, this.y)
       for(var i = 0; i < 30; i+= 1){
         for(var j = 0; j < 14; j+= 1){
-          fill(random(150, 230), random(150, 230), random(150, 230))
+          hue = chroma.hsl(i+j+frameCount % 360, 0.5, 0.7)
+          fill(hue.rgb())
           rect(-75+i*5, -55+j*5, 5, 5)
         }
       }
-      fill(_clr[0], _clr[1], _clr[2])
+      
+      
+      // Weather
       if(toEyeDist < 30){
         if(weather){
           // The eyes will show current weather
           if(rain){
+            hue = chroma.hsl(180+frameCount % 60, 1, 0.7)
+            fill(hue.rgb())
             textSize(70)
             text("☂", -30, 5)
           }
           else if(!rain && !snowfall && cloud >= 50){
+            hue = chroma.hsl(60+frameCount % 60, 1, 0.7)
+            fill(hue.rgb())
             textSize(70)
             text("☁", -30, 5)
           }
           else if(snowfall){
+            hue = chroma.hsl(120+frameCount % 60, 1, 0.7)
+            fill(hue.rgb())
             textSize(70)
             text("❄", -30, 5)
           }
           else{
+            hue = chroma.hsl(frameCount % 60, 1, 0.7)
+            fill(hue.rgb())
             textSize(70)
             text("☀", -30, 5)
           }
         }
         else{
-          fill(_clr[0] + random(0, 50), _clr[1]+ random(0, 50), _clr[2]+ random(0, 50))
+          hue = chroma.hsl(frameCount % 360, 1, 0.7)
+          fill(hue.rgb())
+          //fill(_clr[0] + random(0, 50), _clr[1]+ random(0, 50), _clr[2]+ random(0, 50))
           textFont(myFont1, 60)
           text("CC", -45, 5)
         }
@@ -353,9 +370,121 @@ class axolotl{
       ellipse(76, -20, 38, 80)
       pop()
     }
+    else if(eyes_type%10 == 7){
+      // 8th type of eyes
+      // Three eyes
+      // ( ∴ )
+      push()
+      translate(this.x, this.y)
+      fill('white')
+      ellipse(-40, -20, 50)
+      ellipse(40, -20, 50)
+      fill(_clr[0], _clr[1], _clr[2])
+      ellipse(-40+slight_Xm, -20+slight_Ym, 30)
+      ellipse(40+slight_Xm, -20+slight_Ym, 30)
+      fill('white')
+      ellipse(-40+slight_Xm, -20+slight_Ym, 5+ toEyeDist/100)
+      ellipse(40+slight_Xm, -20+slight_Ym, 5+ toEyeDist/100)
+      fill(face_clr[0], face_clr[1], face_clr[2])
+      push()
+      rotate(-PI/12+map(toEyeDist, 0, 500, 0, PI/12))
+      ellipse(-35+slight_Xm, -50+slight_Ym, 65+slight_Xm, 35)
+      pop()
+      push()
+      rotate(PI/12-map(toEyeDist, 0, 500, 0, PI/12))
+      ellipse(35+slight_Xm, -50+slight_Ym, 65+slight_Xm, 35)
+      pop()
+
+      fill('white')
+      ellipse(0, -50, 50-toEyeDist/80)
+      if(toEyeDist < 60){
+        for(var i=0; i <10; i++){
+          // Halo
+          push()
+          translate(0, -50)
+          rotate(i*PI/10)
+          fill(250, 244, 210, 500)
+          ellipse(0, 0, 33 + random(0, 10), 33)
+          pop()
+        }
+        fill('#DEAB4E')
+        ellipse(0+slight_Xm, -50+slight_Ym, 30-toEyeDist/80+random(0, 3))
+        fill(240, 240, 210, 50)
+        ellipse(0+slight_Xm, -50+slight_Ym, 23-toEyeDist/80+random(0, 3))
+        
+      }
+      else{
+        fill(_clr[1], _clr[0], _clr[2])
+        ellipse(0+slight_Xm, -50+slight_Ym, 30-toEyeDist/80)
+      }
+      fill('white')
+      ellipse(0, -50, 6)
+      fill(face_clr[0], face_clr[1], face_clr[2])
+      ellipse(0+slight_Xm, -70+slight_Ym, 60+slight_Xm, 40)
+      pop()
+    }
+    else if(eyes_type%10 == 8){
+      // 9th type of eyes
+      // Evil eyes
+      // (◣◢)
+      push()
+      translate(this.x, this.y)
+      fill(_clr[0],_clr[1], _clr[2])
+      ellipse(-40+slight_Xm, -30+slight_Ym, 65, 60)
+      ellipse(40+slight_Xm, -30+slight_Ym, 65, 60)
+      fill(_clr[0]+15,_clr[1]+12, _clr[2]+12)
+      ellipse(-40, -30, 50)
+      ellipse(40, -30, 50)
+      fill(_clr[0]+30,_clr[1]+24, _clr[2]+24)
+      ellipse(-40-slight_Xm, -30-slight_Ym, 40)
+      ellipse(40-slight_Xm, -30-slight_Ym, 40)
+      fill(_clr[0]+45,_clr[1]+36, _clr[2]+36)
+      ellipse(-40, -30, 30)
+      ellipse(40, -30, 30)
+      fill('#FBFBFB')
+      ellipse(-40+slight_Xm, -30+slight_Ym, 20, 40)
+      ellipse(40+slight_Xm, -30+slight_Ym, 20, 40)
+      fill(_clr[0]+50,_clr[1]+45, _clr[2]+45)
+      ellipse(-40+slight_Xm, -30+slight_Ym, 10, 30)
+      ellipse(40+slight_Xm, -30+slight_Ym, 10, 30)
+      
+      fill(face_clr[0], face_clr[1], face_clr[2])
+      push()
+      rotate(PI/12)
+      rect(-85,-60, 78,40)
+      pop()
+      push()
+      rotate(-PI/12)
+      rect(7,-60, 78,40)
+      pop()
+      pop()
+    }
+    else if(eyes_type%10 == 9){
+      // 10th type of eyes
+      // Yinyang eyes
+      // (☯)
+      push()
+      translate(this.x, this.y)
+      fill(_clr[0], _clr[1], _clr[2])
+      rect(-80,-40, 70,40, 0, 100, 0, 100)
+      fill('white')
+      rect(-75+slight_Xm/5,-35, 55,30, 0, 100, 0, 100)
+      fill(255-_clr[0], 255-_clr[1], 255-_clr[2])
+      rect(10,-40, 70,40, 100, 0, 100, 0)
+      fill('white')
+      rect(20+slight_Xm/5,-35, 55,30, 100, 0, 100, 0)
+      fill(255-_clr[0], 255-_clr[1], 255-_clr[2])
+      ellipse(-45+slight_Xm, -20+slight_Ym/2, 24, 23)
+      fill(_clr[0], _clr[1], _clr[2])
+      ellipse(45+slight_Xm, -20+slight_Ym/2, 24, 23)
+      fill('white')
+      ellipse(-45+slight_Xm/2, -22+slight_Ym, 5)
+      ellipse(45+slight_Xm/2, -22+slight_Ym, 5)
+      pop()
+    }
   }
   
-  mouth(mouth_type, _clr, face_clr){
+   mouth(mouth_type, _clr, face_clr){
     noStroke()
     fill(_clr[0], _clr[1], _clr[2])
     var slight_Xm = map(mouseX - this.x, -width/2, width/2, -5, 5, true)
@@ -485,7 +614,7 @@ class axolotl{
         ellipse(10, -150, 50)
       }
       // Show temperature
-      if(dist(mouseX, mouseY, this.x, this.y-150) < 60 && eyes_type%7 == 6){
+      if(dist(mouseX, mouseY, this.x, this.y-150) < 60 && eyes_type%10 == 6){
         fill("white")
         textFont(myFont1, 40)
         if(temp >= 100 || temp <= -10){
@@ -532,7 +661,7 @@ function preload(){
 }
 
 function setup() {
-  createCanvas(600, 500)
+  createCanvas(800, 600)
   frameRate(60)
   apiRequest()
 }
@@ -549,14 +678,20 @@ var weather = false
 
 function draw() {
   // Build the background using a halo effect and the face color
-  background(158)
+  background(160)
   blendMode(MULTIPLY) // MULTIPLY - multiply the colors, result will always be darker
   var bg_clr1 = color(face_clr[0], face_clr[1], face_clr[2])
   noStroke()
   for(var i = 0; i < width*2; i+= 56){
-      bg_clr1.setAlpha(36)
-      fill(bg_clr1)
-      ellipse(width/2,height/2, i+mouseX)
+    bg_clr1.setAlpha(26)
+    fill(bg_clr1)
+    ellipse(width/2,height/2, i+mouseX)
+    // rect(0,0, i+mouseX, i+mouseX)
+    // push()
+    // translate(width/2, height/2)
+    // rotate(PI)
+    // rect(-width/2,-height/2, i+mouseX, i+mouseX)
+    // pop()
   }
   
   
